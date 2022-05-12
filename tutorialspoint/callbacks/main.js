f8182ad639a2f1c7b28ceff25df8aab5fefd3abf
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-05-12 10:49:09 +0700

    learn callback

---
+++ tutorialspoint/callbacks/main.js
@@ -0,0 +1,17 @@
+// Example 1:
+// var fs = require("fs");
+// var data = fs.readFileSync('input.txt');
+
+// console.log(data.toString());
+// console.log('Program ended.');
+
+// Example 2:
+var fs = require("fs");
+fs.readFile('input.txt', (err, data) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(data.toString());
+});
+
+console.log('Program ended.');
